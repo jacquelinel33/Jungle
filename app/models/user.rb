6d7137be722e4c3475029d9b236bde337f87598b
Jacqueline <jlee4332@gmail.com>
2021-05-04 13:14:33 -0400

    add authenticate with credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,18 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 3 }
   validates :password_confirmation, presence: true
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.strip.downcase
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 
 end
--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-
+    
     before :each do
-      @user = User.new(:first_name => 'John', :last_name => 'Smith', :email => 'test@gmail.com', :password => 'test', :password_confirmation => 'test')
+      @user = User.new(:first_name => 'John', :last_name => 'Smith', :email => 'test@gmail.com', :password => 'tests', :password_confirmation => 'tests')
     end
 
     it 'should save user is all params are set' do
@@ -37,20 +37,61 @@ RSpec.describe User, type: :model do
       @user.password_confirmation = 'wrongpw'
       @user.save
       expect(@user).to_not be_valid
-      expect(@user.errors.full_messages).not_to  be_empty
+      expect(@user.errors.full_messages).not_to be_empty
     end
     
     it 'should not validate if there are no passwords entered' do
-      @user.password_confirmation = 'wrongpw'
+      @user.password = nil
       @user.save
       expect(@user).to_not be_valid
-      expect(@user.errors.full_messages).not_to  be_empty
+      expect(@user.errors.full_messages).not_to be_empty
     end
 
-    # it 'should not validate if email is not unique' do
-    # end
+    it 'should not validate if email is not unique' do
+      @user.save
+      @user2 = User.new(:first_name => 'Jane', :last_name => 'Doe', :email => 'test@gmail.com', :password => 'tests', :password_confirmation => 'tests')
+      @user2.save
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).not_to  be_empty
+    end
 
-#   it 'should '
-  # end
+  it 'should not validate if passwords are less than minimum length of 3' do
+    @user.password = 't'
+    expect(@user).to_not be_valid
+    expect(@user.errors.full_messages).not_to be_empty
+  end
+end
+
+  describe '.authenticate_with_credentials' do
+
+    before :each do
+      @user = User.new(:first_name => 'John', :last_name => 'Smith', :email => 'testing@gmail.com', :password => 'tests', :password_confirmation => 'tests')
+    end
+
+    it 'should not return nil if successfully authenticated' do
+      @user.save
+      @user = User.authenticate_with_credentials('testing@gmail.com', 'tests')
+      expect(@user).not_to be(nil)
+    end
+
+    it 'should return nil if unsuccessfully authenticated' do
+      @user.save
+      @user = User.authenticate_with_credentials('testing@gmail.com', 'wrongpw')
+      expect(@user).to be(nil)
+    end
+
+    it 'should not be nil if email is typed with a few spaces before and after email address' do
+      @user.save
+      @user = User.authenticate_with_credentials('testing@gmail.com    ', 'tests')
+      expect(@user).not_to be(nil)
+    end
+
+    it 'should not be nil is visitor types in wrong email case' do
+      @user.save
+      @user = User.authenticate_with_credentials('TESting@gmail.com', 'tests')
+      expect(@user).not_to be(nil)
+
+    end
+
   end
 end
